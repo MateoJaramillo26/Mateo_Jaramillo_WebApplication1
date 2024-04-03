d578092284a971d7ce5a547d055721a33617a87e
MateoJaramillo <mateo.jaramillo.costa@udla.edu.ec>
2024-04-03 11:47:09 -0500

    Add project files.

---
+++ Mateao_Jaramillo_WebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mateao_Jaramillo_WebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
